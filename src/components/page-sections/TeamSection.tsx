493ccb415b0d54dead46c309a8f2d7a5d8f9fc69
Klink <85062+dogmar@users.noreply.github.com>
2023-06-27 20:08:13 -0700

    Team member cards finished

--- src/components/page-sections/TeamSection.tsx
+++ src/components/page-sections/TeamSection.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import { ColorModeProvider } from '@pluralsh/design-system'
+
 import classNames from 'classnames'
 import styled from 'styled-components'
 
@@ -17,11 +19,45 @@ function getImageUrl(image: ImageFileFragment) {
   return `${FILE_PREFIX}/${image.filename_disk}/${image.filename_download}`
 }
 
-const MemberSC = styled.li(({ theme: _ }) => ({
+const MemberSC = styled.li(({ theme }) => ({
+  position: 'relative',
+  overflow: 'hidden',
+  borderRadius: theme.borderRadiuses.medium,
+  paddingBottom: '138.889%',
+  '.bg': {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+  },
   img: {
     display: 'block',
     width: '100%',
   },
+}))
+
+const MemberInfoSC = styled.div(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  rowGap: theme.spacing.xsmall,
+  borderTopLeftRadius: theme.borderRadiuses.medium,
+  borderTopRightRadius: theme.borderRadiuses.medium,
+  borderTop: theme.borders['fill-three'],
+  borderLeft: theme.borders['fill-three'],
+  borderRight: theme.borders['fill-three'],
+  color: theme.colors.text,
+  position: 'absolute',
+  bottom: 0,
+  left: theme.spacing.medium,
+  right: theme.spacing.medium,
+  backdropFilter: 'blur(25px)',
+  background: 'rgba(33,36,44, 0.4)',
+  paddingTop: theme.spacing.xlarge,
+  paddingLeft: theme.spacing.medium,
+  paddingRight: theme.spacing.medium,
+  paddingBottom: theme.spacing.small,
 }))
 
 function Member({ member, ...props }: { member: TeamMemberFragment }) {
@@ -29,8 +65,34 @@ function Member({ member, ...props }: { member: TeamMemberFragment }) {
 
   return (
     <MemberSC {...props}>
-      {member.portrait && <img src={getImageUrl(member.portrait)} />}
-      <div>{member.name}</div>
+      {member.portrait && (
+        <div
+          className="bg"
+          style={{ backgroundImage: `url(${getImageUrl(member.portrait)})` }}
+        />
+      )}
+      <ColorModeProvider mode="dark">
+        <MemberInfoSC>
+          <ResponsiveText
+            as="h4"
+            textStyles={{ '': 'mSubtitle1' }}
+          >
+            {member.name}
+          </ResponsiveText>
+          <ResponsiveText
+            as="p"
+            textStyles={{ '': 'aOverline' }}
+          >
+            {member.pronouns}
+          </ResponsiveText>
+          <ResponsiveText
+            as="p"
+            textStyles={{ '': 'mLabel' }}
+          >
+            {member.title}
+          </ResponsiveText>
+        </MemberInfoSC>
+      </ColorModeProvider>
     </MemberSC>
   )
 }
