41b2ec774c6c91a48c5717e3bf8dc1090a149bae
Sam Bolian <s.woodward90@gmail.com>
2024-01-04 15:13:37 -0700

    line break for lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -350,7 +350,8 @@ function BuildSecurely() {
         heading="Secure, flexible, and easy"
         intro={
           <div className="[text-wrap:balance]">
-            Secure and scalable pull-based architecture. A single pane of glass to understand and maintain complex Kubernetes fleets.
+            Secure and scalable pull-based architecture. A single pane of glass 
+            to understand and maintain complex Kubernetes fleets.
           </div>
         }
       />
