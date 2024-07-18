fe7e88a20b7170bb06ba51497fb238f627ca0acf
Nick Cernera <nickcernera@fastmail.com>
2024-07-18 12:26:12 -0400

    fix: make the quotes consistent with each other

--- pages/kubernetes-fleet-management.tsx
+++ pages/kubernetes-fleet-management.tsx
@@ -85,8 +85,7 @@ export default function Legal(
         title="Delivering value to DevOps and platform engineering teams"
         quote={`By adopting Plural for our Kubernetes fleet management solution, we
         reduced our k8s upgrade cycle from 3 months to 1 day, enabling us to
-        delegate responsibilities more effectively from principal engineers to
-        mid-level engineer`}
+        delegate responsibilities more effectively from principal engineers to a single mid-level engineer.`}
         attribution="Director DevOps, Leading Global Cybersecurity Provider"
       />
       <ArticleSection articleCards={_props.articleCards} />
