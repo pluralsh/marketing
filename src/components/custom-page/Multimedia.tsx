74602127249d3d9c5229ee633effe280eaee466d
Jake Laderman <jsladerman@gmail.com>
2024-11-22 17:16:30 -0500

    fix forms to enable hubspot

--- src/components/custom-page/Multimedia.tsx
+++ src/components/custom-page/Multimedia.tsx
@@ -1,7 +1,5 @@
 import { type ComponentPropsWithRef } from 'react'
 
-import { Code } from '@pluralsh/design-system'
-
 import styled from 'styled-components'
 
 import { getImageUrl } from '@src/consts/routes'
@@ -47,7 +45,27 @@ export function Multimedia({
           url={videoUrl ?? ''}
         />
       ) : mediaType === 'form' ? (
-        <Code css={{ overflow: 'auto', maxHeight: '500px' }}>{form ?? ''}</Code>
+        <iframe
+          title="Embedded form content"
+          css={{
+            width: '100%',
+            height: '100%',
+            minHeight: 550,
+          }}
+          sandbox="allow-scripts allow-forms allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+          srcDoc={`
+            <!DOCTYPE html>
+            <html>
+            <head>
+              <!-- HubSpot's required script -->
+              <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+            </head>
+            <body>
+            ${form}
+            </body>
+            </html>
+          `}
+        />
       ) : null}
     </MultimediaWrapperSC>
   )
