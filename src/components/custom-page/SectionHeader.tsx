c75662e2fec3bb7f2f5ca5777a45c5f80f79d9a6
Jake Laderman <jsladerman@gmail.com>
2024-11-27 12:33:59 -0600

    improve section header

--- src/components/custom-page/SectionHeader.tsx
+++ src/components/custom-page/SectionHeader.tsx
@@ -8,14 +8,16 @@ export function SectionHeader({
   description,
 }: SectionHeaderComponentFragment) {
   return (
-    <div className="mx-auto flex w-1/2 flex-col items-center gap-medium py-xlarge text-center">
-      {overline && <OverlineLabel>{overline}</OverlineLabel>}
-      {title && (
-        <ResponsiveText textStyles={{ lg: 'mHero2', '': 'mTitle1' }}>
-          {title}
-        </ResponsiveText>
-      )}
-      <Body1 $color="text-light">{description}</Body1>
+    <div className="flex w-full flex-col items-center justify-center py-xlarge">
+      <div className="flex flex-col gap-medium text-center lg:min-w-[400px] lg:max-w-[624px]">
+        {overline && <OverlineLabel>{overline}</OverlineLabel>}
+        {title && (
+          <ResponsiveText textStyles={{ lg: 'mHero2', '': 'mTitle1' }}>
+            {title}
+          </ResponsiveText>
+        )}
+        <Body1 $color="text-light">{description}</Body1>
+      </div>
     </div>
   )
 }
