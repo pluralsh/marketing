feeecce53e59ad40b85e412f40e89eca20431383
Jake Laderman <jsladerman@gmail.com>
2025-08-25 14:17:44 -0400

    remove prismic dev tools from prod site (#169)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
 import createMiddleware from 'next-intl/middleware'
 
@@ -9,7 +10,23 @@ import {
 
 import { HEADERS } from './constants'
 
+const PROD_BLOCK_LIST = ['/api/preview', '/slice-library', '/slice-simulator']
+const I18N_SKIP_LIST = ['/api', '/slice-library', '/slice-simulator', '/_next']
+
 export default async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl
+
+  // block prismic-related stuff on main prod site
+  if (
+    isMainProdSite(request) &&
+    PROD_BLOCK_LIST.some((route) => pathname.startsWith(route))
+  )
+    return NextResponse.redirect(new URL('/not-found', request.url))
+
+  // skip i18n middleware for API routes and Next internal files/static assets
+  if (I18N_SKIP_LIST.some((route) => pathname.startsWith(route)))
+    return NextResponse.next()
+
   const [locales, defaultLocale] = await Promise.all([
     getPrismicLanguages(),
     getPrismicDefaultLanguage(),
@@ -27,7 +44,11 @@ export default async function middleware(request: NextRequest) {
   return response
 }
 
-export const config = {
-  // Skip all these paths when matching. Add all paths that are siblings to [locale] folder.
-  matcher: ['/((?!api|_next|_vercel|slice-library|slice-simulator|.*\\..*).*)'],
+const isMainProdSite = (request: NextRequest) => {
+  const requestOrigin = cleanUrl(request.nextUrl.origin)
+  const prodOrigin = cleanUrl(process.env.NEXT_PUBLIC_SITE_URL)
+  return !!requestOrigin && !!prodOrigin && requestOrigin === prodOrigin
 }
+
+const cleanUrl = (url: string | null | undefined) =>
+  url ? url.replace(/\/+$/, '') : undefined
