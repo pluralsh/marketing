daa3c4af057702794ca8d0172d5005040881dbb5
Jake Laderman <jsladerman@gmail.com>
2025-08-27 16:21:23 -0400

    more test

--- src/middleware.ts
+++ src/middleware.ts
@@ -54,5 +54,5 @@ const isMainProdSite = (request: NextRequest) => {
   return !!requestOrigin && !!prodOrigin && requestOrigin === prodOrigin
 }
 
-const cleanUrl = (url: string | null | undefined) =>
-  url ? url.replace(/\/+$/, '') : undefined
+const cleanUrl = (url: string | null | undefined): string | undefined =>
+  url ? url.replace(/^https?:\/\//, '').replace(/\/+$/, '') : undefined
